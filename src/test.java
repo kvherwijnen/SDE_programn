14fa3b4193a4ac0efdd89bb94d9ef7778137671e
Kevin van Herwijnen <kvherwijnen@hotmail.com>
2023-01-21 19:03:03 +0100

    create merge conflicts

--- src/test.java
+++ src/test.java
@@ -8,19 +8,16 @@ public class test {
 
     @org.junit.Test
     public void testCreateUserCommand() {
-        // set up input for the test
+
         String input = "create\nJohn Doe\nadmin\nexit\n";
         ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
-        // set up output for the test
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
 
-        // run the main method of the DesignPatterns class
         DesignPatterns.main(new String[]{});
 
-        // check if the output is as expected
         String output = out.toString();
         assertTrue(output.contains("Enter command (create, exit): User created: John Doe (Admin)"));
         assertTrue(output.contains("Enter command (create, exit): Exiting program..."));
